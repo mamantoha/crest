3977e003c886d7d40d49a50400581a7935c18f82
Anton Maminov <anton.maminov@gmail.com>
2017-04-04 14:20:18 +0300

    remove integration test from unit

--- spec/unit/crest.cr
+++ spec/unit/crest.cr
@@ -1,11 +1,1 @@
 require "../spec_helper"
-
-describe Crest do
-  it "do GET request" do
-    response = Crest.get("http://localhost")
-  end
-
-  it "do POST request" do
-    response = Crest.post("http://localhost", {:foo => "bar"})
-  end
-end
