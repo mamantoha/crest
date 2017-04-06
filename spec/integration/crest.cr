adfa3e1a623ec65d5b9b8cbf85a074a64465f554
Anton Maminov <anton.maminov@gmail.com>
2017-04-06 19:47:26 +0300

    check when spec fail

--- spec/integration/crest.cr
+++ spec/integration/crest.cr
@@ -3,7 +3,7 @@ require "../spec_helper"
 describe Crest do
   it "do GET request" do
     response = Crest.get("#{TEST_SERVER_URL}")
-    (response.body).should eq("Hello World!")
+    (response.body).should eq("Hello World!1")
   end
 
   it "upload file" do
