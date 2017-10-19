bee081cbde03dca4b83997419a230015f7e44dfa
Anton Maminov <anton.maminov@gmail.com>
2017-10-19 10:47:46 +0300

    add version file

--- spec/unit/crest_spec.cr
+++ spec/unit/crest_spec.cr
@@ -1,1 +1,7 @@
 require "../spec_helper"
+
+describe Crest::VERSION do
+  it "should have version" do
+    (Crest::VERSION).should_not be_nil
+  end
+end
---
+++ src/crest/version.cr
@@ -0,0 +1,3 @@
+module Crest
+  VERSION = "0.0.1"
+end
