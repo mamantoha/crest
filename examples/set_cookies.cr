07d97c95c3c82699f55b8aa823abe8fdb45ba4e0
Anton Maminov <anton.linux@gmail.com>
2017-10-22 15:53:38 +0300

    update readme

--- examples/set_cookies.cr
+++ examples/set_cookies.cr
@@ -1,5 +1,14 @@
 require "../src/crest"
 
+url = "http://httpbin.org/cookies"
+res = Crest.get(url, cookies: {"v1" => "k1"})
+puts "Body:"
+puts res.body
+puts "Headers:"
+puts res.headers
+puts "Cookies:"
+puts res.cookies
+
 params = {"k1" => "v1", "k2" => "v2"}
 url = "http://httpbin.org/cookies/set"
 res = Crest.get(url, params: params)
@@ -8,4 +17,13 @@ puts res.body
 puts "Headers:"
 puts res.headers
 puts "Cookies:"
-puts res.request_cookies
+puts res.cookies
+
+url = "http://httpbin.org/headers"
+res = Crest.get(url, headers: {"Authorization" => "Bearer cT0febFoD5lxAlNAXHo6g"})
+puts "Body:"
+puts res.body
+puts "Headers:"
+puts res.headers
+puts "Cookies:"
+puts res.cookies
