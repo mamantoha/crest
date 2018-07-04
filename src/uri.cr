ffad75c9ce304f3237e83a544f3832be29883150
Anton Maminov <anton.linux@gmail.com>
2018-07-04 13:25:16 +0300

    change URI#absolute?

--- src/uri.cr
+++ src/uri.cr
@@ -1,7 +1,7 @@
 class URI
   # TODO remove after https://github.com/crystal-lang/crystal/pull/6311 will be merged and released
   def absolute?
-    @scheme && (((host = @host) && !host.empty?) || ((path = @path) && !path.empty?)) ? true : false
+    @scheme ? true : false
   end
 
   def relative?
