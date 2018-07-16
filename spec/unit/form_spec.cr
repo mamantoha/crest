275b2baf0abf60ac2acab97bdf08285fa0774f36
Anton Maminov <anton.linux@gmail.com>
2018-07-16 22:14:40 +0300

    test generate form with multipart

--- spec/unit/form_spec.cr
+++ spec/unit/form_spec.cr
@@ -13,5 +13,19 @@ describe Crest::Form do
       form.params.should eq(input)
       form.parsed_params.should eq(parsed_input)
     end
+
+    it "generate form with multipart" do
+      file = File.open("#{__DIR__}/../support/fff.png")
+      input = {:file => file, "param1" => "value1"}
+
+      parsed_input = [{"file", file}, {"param1", "value1"}]
+      content_type = "multipart/form-data"
+
+      form = Crest::Form.generate(input)
+
+      form.content_type.should contain(content_type)
+      form.params.should eq(input)
+      form.parsed_params.should eq(parsed_input)
+    end
   end
 end
