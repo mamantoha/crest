5d958dd9d571bf44531ccbba5e74a5499d3d8081
Anton Maminov <anton.linux@gmail.com>
2018-07-17 14:33:47 +0300

    abstract class Form

--- src/crest/forms/urlencoded_form.cr
+++ src/crest/forms/urlencoded_form.cr
@@ -1,20 +1,12 @@
+require "../form"
+
 module Crest
   # This class lets `crest` emulate a filled-in form
   # in which a user has pressed the submit button.
   # This causes `crest` to POST data using the
   # "Content-Type" `application/x-www-form-urlencoded`.
-  class UrlencodedForm(T)
-    @form_data : String = ""
+  class UrlencodedForm(T) < Crest::Form(T)
     @content_type : String = "application/x-www-form-urlencoded"
-
-    getter params, form_data, content_type
-
-    def self.generate(params : Hash)
-      new(params).generate
-    end
-
-    def initialize(@params : T)
-    end
 
     def generate
       @form_data = parsed_params
