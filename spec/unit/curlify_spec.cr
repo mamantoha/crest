79c348ed88b2f546c384575ae320645c708151ec
Anton Maminov <anton.linux@gmail.com>
2018-07-17 16:18:37 +0300

    update to crest 0.12.0

--- spec/unit/curlify_spec.cr
+++ spec/unit/curlify_spec.cr
@@ -20,21 +20,29 @@ describe Crest::Curlify do
   end
 
   it "converts POST request" do
-    request = Crest::Request.new(:post, "http://httpbin.org/post", payload: {"param1" => "value1", "param2" => "value2"})
+    request = Crest::Request.new(:post, "http://httpbin.org/post", form: {"param1" => "value1", "param2" => "value2"})
 
-    result = "curl -X POST http://httpbin.org/post -F 'param1=value1' -F 'param2=value2' -H 'Content-Type: multipart/form-data'"
+    result = "curl -X POST http://httpbin.org/post -d 'param1=value1&param2=value2' -H 'Content-Type: application/x-www-form-urlencoded'"
+    curlify(request).should eq(result)
+  end
+
+  it "converts POST request with multipart" do
+    file = File.open("#{__DIR__}/../support/fff.png")
+    request = Crest::Request.new(:post, "http://httpbin.org/post", form: {"param1" => "value1", "file" => file})
+
+    result = "curl -X POST http://httpbin.org/post -F 'param1=value1' -F 'file=#{"#{__DIR__}/../support/fff.png"}' -H 'Content-Type: multipart/form-data'"
     curlify(request).should eq(result)
   end
 
   it "converts POST request with headers" do
-    request = Crest::Request.new(:post, "http://httpbin.org/post", payload: {"param1" => "value1"}, headers: {"user-agent" => "crest"})
+    request = Crest::Request.new(:post, "http://httpbin.org/post", form: {"param1" => "value1"}, headers: {"user-agent" => "crest"})
 
-    result = "curl -X POST http://httpbin.org/post -F 'param1=value1' -H 'user-agent: crest' -H 'Content-Type: multipart/form-data'"
+    result = "curl -X POST http://httpbin.org/post -d 'param1=value1' -H 'user-agent: crest' -H 'Content-Type: application/x-www-form-urlencoded'"
     curlify(request).should eq(result)
   end
 
   it "converts POST request with json" do
-    request = Crest::Request.new(:post, "http://httpbin.org/post", payload: {:foo => "bar"}.to_json, headers: {"Content-Type" => "application/json"})
+    request = Crest::Request.new(:post, "http://httpbin.org/post", form: {:foo => "bar"}.to_json, headers: {"Content-Type" => "application/json"})
 
     result = "curl -X POST http://httpbin.org/post -d '{\"foo\":\"bar\"}' -H 'Content-Type: application/json'"
 
