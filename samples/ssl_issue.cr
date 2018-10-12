19427c91f44c4e15db2f8044ef089201fc0b4839
Anton Maminov <anton.linux@gmail.com>
2018-10-12 12:18:08 +0300

    fix ameba warnings

--- samples/ssl_issue.cr
+++ samples/ssl_issue.cr
@@ -5,15 +5,15 @@ tls = OpenSSL::SSL::Context::Client.insecure
 # HTTP::Client
 uri = URI.parse("https://expired.badssl.com")
 client = HTTP::Client.new(uri, tls: tls)
-response = client.get("/")
+client.get("/")
 
 # Crest
-response = Crest.get("https://expired.badssl.com", tls: tls)
+Crest.get("https://expired.badssl.com", tls: tls)
 
 # Crest::Request
 request = Crest::Request.new(:get, "https://expired.badssl.com", tls: tls)
-response = request.execute
+request.execute
 
 # Crest::Resource
 site = Crest::Resource.new("https://expired.badssl.com", tls: tls)
-response = site.get("/")
+site.get("/")
