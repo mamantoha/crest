50f4586d361026f35dc2298298c01133fd2ca21b
Anton Maminov <anton.linux@gmail.com>
2018-11-15 21:44:06 +0200

    change proxy client

--- src/http/proxy/client.cr
+++ src/http/proxy/client.cr
@@ -19,33 +19,34 @@ module HTTP
 
       getter tls : OpenSSL::SSL::Context::Client?
 
-      # Create a new socket factory that tunnels via the given host and  port.
+      @dns_timeout : Float64?
+      @connect_timeout : Float64?
+      @read_timeout : Float64?
+
+      record Response,
+        version : String,
+        code : Int32,
+        reason : String,
+        headers = {} of String => String
+
+      # Creates a new socket factory that tunnels via the given host and port.
       # The following optional arguments are supported:
       #
-      # * `username` - the user name to use when authenticating to the proxy
-      # * `password` - the password to use when authenticating
-      def initialize(
-        @host : String,
-        @port = 80,
-        *,
-        @username : String?,
-        @password : String?
-      )
+      # * :username - the user name to use when authenticating to the proxy
+      # * :password - the password to use when authenticating
+      def initialize(@host, @port, *, @username = nil, @password = nil)
       end
 
-      # Return a new socket connected to the given host and port via the
+      # Returns a new socket connected to the given host and port via the
       # proxy that was requested when the socket factory was instantiated.
-      def open(host, port, tls = nil, connection_options = {} of Symbol => Float64 | Nil)
-        dns_timeout = connection_options.fetch(:dns_timeout, nil)
-        connect_timeout = connection_options.fetch(:connect_timeout, nil)
-        read_timeout = connection_options.fetch(:read_timeout, nil)
-
-        socket = TCPSocket.new(@host, @port, dns_timeout, connect_timeout)
-        socket.read_timeout = read_timeout if read_timeout
+      def open(host, port, tls = nil, *, @dns_timeout, @connect_timeout, @read_timeout)
+        socket = TCPSocket.new(@host, @port, @dns_timeout, @connect_timeout)
+        socket.read_timeout = @read_timeout if @read_timeout
         socket.sync = true
 
         if tls
           socket << "CONNECT #{host}:#{port} HTTP/1.0\r\n"
+          socket << "Host: #{host}:#{port}\r\n"
 
           if @username
             credentials = Base64.strict_encode("#{@username}:#{@password}")
@@ -57,7 +58,7 @@ module HTTP
 
           resp = parse_response(socket)
 
-          if resp[:code]? == 200
+          if resp.code == 200
             {% if !flag?(:without_openssl) %}
               if tls
                 tls_socket = OpenSSL::SSL::Socket::Client.new(socket, context: tls, sync_close: true, hostname: host)
@@ -75,28 +76,17 @@ module HTTP
         socket
       end
 
-      private def parse_response(socket)
-        resp = {} of Symbol => Int32 | String | Hash(String, String)
+      private def parse_response(socket) : Response?
+        version, code, reason = socket.gets.as(String).chomp.split(/ /, 3)
 
-        begin
-          version, code, reason = socket.gets.as(String).chomp.split(/ /, 3)
+        headers = {} of String => String
 
-          headers = {} of String => String
-
-          while (line = socket.gets.as(String)) && (line.chomp != "")
-            puts line
-            name, value = line.split(/:/, 2)
-            headers[name.strip] = value.strip
-          end
-
-          resp[:version] = version
-          resp[:code] = code.to_i
-          resp[:reason] = reason
-          resp[:headers] = headers
-        rescue
+        while (line = socket.gets.as(String)) && (line.chomp != "")
+          name, value = line.split(/:/, 2)
+          headers[name.strip] = value.strip
         end
 
-        return resp
+        Response.new(version, code.to_i, reason, headers)
       end
     end
   end
@@ -106,28 +96,27 @@ module HTTP
       return unless proxy
 
       begin
-        @socket = proxy.open(host: @host, port: @port, tls: @tls, connection_options: proxy_connection_options)
+        @socket = proxy.open(
+          host: @host,
+          port: @port,
+          tls: @tls,
+          dns_timeout: @dns_timeout,
+          connect_timeout: @connect_timeout,
+          read_timeout: @read_timeout
+        )
+      rescue ex : IO::Error
+        raise IO::Error.new("Failed to open TCP connection to #{@host}:#{@port} (#{ex.message})")
+      end
 
-        if proxy.username
-          proxy_basic_auth(proxy.username, proxy.password)
-        end
-      rescue IO::Error
-        @socket = nil
+      if proxy.username && proxy.password
+        proxy_basic_auth(proxy.username, proxy.password)
       end
 
       @socket
     end
 
-    def proxy_connection_options
-      opts = {} of Symbol => Float64 | Nil
-
-      opts[:dns_timeout] = @dns_timeout
-      opts[:connect_timeout] = @connect_timeout
-      opts[:read_timeout] = @read_timeout
-
-      return opts
-    end
-
+    # Configures this client to perform proxy basic authentication in every
+    # request.
     private def proxy_basic_auth(username : String?, password : String?)
       header = "Basic #{Base64.strict_encode("#{username}:#{password}")}"
       before_request do |request|
