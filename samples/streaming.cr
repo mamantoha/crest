c818027bfd3e8d495c73bdb2f2002cf251dd0d67
Anton Maminov <anton.linux@gmail.com>
2018-12-25 12:58:12 +0200

    resource streaming sample

--- samples/streaming.cr
+++ samples/streaming.cr
@@ -9,6 +9,14 @@ response = Crest::Request.get("http://httpbin.org/stream/5") do |resp|
 end
 response # => nil
 
+resource = Crest::Resource.new("http://httpbin.org")
+resource["/stream/5"].get do |resp|
+  puts resp.status_code # => 200
+  resp.body_io.each_line do |line|
+    puts JSON.parse(line)
+  end
+end
+
 Crest.get("https://github.com/mamantoha/crest/archive/v0.17.0.zip") do |resp|
   filename = resp.filename || "crest.zip"
 
