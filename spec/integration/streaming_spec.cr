735fd45ef610978cdde581c9d628dde4926d4c44
Anton Maminov <anton.linux@gmail.com>
2018-12-26 16:26:39 +0200

    add stream endpoint to test server

--- spec/integration/streaming_spec.cr
+++ spec/integration/streaming_spec.cr
@@ -3,8 +3,9 @@ require "../spec_helper"
 describe Crest do
   describe "Streaming" do
     it "should stream Response#execute" do
+      count = 5
       body = String::Builder.new
-      request = Crest::Request.new(:get, "#{TEST_SERVER_URL}/")
+      request = Crest::Request.new(:get, "#{TEST_SERVER_URL}/stream/#{count}")
 
       request.execute do |resp|
         while line = resp.body_io.gets
@@ -12,64 +13,69 @@ describe Crest do
         end
       end
 
-      body.to_s.should eq("Hello World!")
+      body.to_s.should eq("Hello World!" * count)
     end
 
     it "should stream Request.execute" do
+      count = 5
       body = String::Builder.new
-      Crest::Request.get("#{TEST_SERVER_URL}/") do |resp|
+      Crest::Request.get("#{TEST_SERVER_URL}/stream/#{count}") do |resp|
         while line = resp.body_io.gets
           body << line
         end
       end
 
-      body.to_s.should eq("Hello World!")
+      body.to_s.should eq("Hello World!" * count)
     end
 
     it "should stream Request.get" do
+      count = 5
       body = String::Builder.new
-      Crest::Request.execute(:get, "#{TEST_SERVER_URL}/") do |resp|
+      Crest::Request.execute(:get, "#{TEST_SERVER_URL}/stream/#{count}") do |resp|
         while line = resp.body_io.gets
           body << line
         end
       end
 
-      body.to_s.should eq("Hello World!")
+      body.to_s.should eq("Hello World!" * count)
     end
 
     it "should stream Crest#get" do
+      count = 5
       body = String::Builder.new
-      Crest.get("#{TEST_SERVER_URL}/") do |resp|
+      Crest.get("#{TEST_SERVER_URL}/stream/#{count}") do |resp|
         while line = resp.body_io.gets
           body << line
         end
       end
 
-      body.to_s.should eq("Hello World!")
+      body.to_s.should eq("Hello World!" * count)
     end
 
     it "should stream Resource#get with []" do
+      count = 5
       body = String::Builder.new
       resource = Crest::Resource.new(TEST_SERVER_URL)
-      resource["/"].get do |resp|
+      resource["/stream/#{count}"].get do |resp|
         while line = resp.body_io.gets
           body << line
         end
       end
 
-      body.to_s.should eq("Hello World!")
+      body.to_s.should eq("Hello World!" * count)
     end
 
     it "should stream Resource#get" do
+      count = 5
       body = String::Builder.new
       resource = Crest::Resource.new(TEST_SERVER_URL)
-      resource.get("/") do |resp|
+      resource.get("/stream/#{count}") do |resp|
         while line = resp.body_io.gets
           body << line
         end
       end
 
-      body.to_s.should eq("Hello World!")
+      body.to_s.should eq("Hello World!" * count)
     end
 
     it "should stream Crest#get with redirects" do
@@ -85,8 +91,9 @@ describe Crest do
     end
 
     it "should stream Response#execute with redirects" do
+      count = 5
       body = String::Builder.new
-      request = Crest::Request.new(:get, "#{TEST_SERVER_URL}/redirect/2")
+      request = Crest::Request.new(:get, "#{TEST_SERVER_URL}/redirect_stream/#{count}")
 
       request.execute do |resp|
         while line = resp.body_io.gets
@@ -94,19 +101,20 @@ describe Crest do
         end
       end
 
-      body.to_s.should eq("Hello World!")
+      body.to_s.should eq("Hello World!" * 5)
     end
 
     it "should stream Resource#get with redirects" do
+      count = 5
       body = String::Builder.new
       resource = Crest::Resource.new(TEST_SERVER_URL)
-      resource["/redirect/2"].get do |resp|
+      resource["/redirect_stream/#{count}"].get do |resp|
         while line = resp.body_io.gets
           body << line
         end
       end
 
-      body.to_s.should eq("Hello World!")
+      body.to_s.should eq("Hello World!" * count)
     end
   end
 end
