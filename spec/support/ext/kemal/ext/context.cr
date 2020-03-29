345122f8071e8d8dc75fb7d117045755119712e5
Anton Maminov <anton.linux@gmail.com>
2020-03-29 13:37:33 +0300

    add todo about PR to Kemal

--- spec/support/ext/kemal/ext/context.cr
+++ spec/support/ext/kemal/ext/context.cr
@@ -1,5 +1,6 @@
 class HTTP::Server
   class Context
+    # TODO https://github.com/kemalcr/kemal/pull/561
     def redirect(url : String, status_code : Int32 = 302, *, body : String = "")
       @response.headers.add "Location", url
       @response.write(body.to_slice)
