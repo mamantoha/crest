752bdf34366b91dd5e1f6af7ccc557a03389be5c
Chao Yang <cyang05@outlook.com>
2022-06-08 22:39:07 +0000

    Allow JSON::Any in params

--- src/crest/params_encoders/enumerated_flat_params_encoder.cr
+++ src/crest/params_encoders/enumerated_flat_params_encoder.cr
@@ -23,7 +23,7 @@ module Crest
         processed_key = parent_key ? "#{parent_key}[#{k}]" : k.to_s
 
         case v
-        when Hash, Array
+        when Hash, Array, JSON::Any
           memo += flatten_params(v, processed_key)
         else
           memo << {processed_key, v}
@@ -36,15 +36,30 @@ module Crest
     # # => [{"key1[arr][1]", "1"}, {"key1[arr][2]", "2"}, {"key1[arr][3]", "3"}]
     # ```
     def self.flatten_params(object : Array, parent_key : String? = nil) : Array(Tuple(String, Crest::ParamsValue))
-      object.each_with_index(1).reduce([] of Tuple(String, Crest::ParamsValue)) do |memo, (item, index)|
+      object.each_with_index(0).reduce([] of Tuple(String, Crest::ParamsValue)) do |memo, (item, index)|
         processed_key = parent_key ? "#{parent_key}[#{index}]" : ""
 
         case item
-        when Hash
+        when Hash, JSON::Any
           memo += flatten_params(item, processed_key)
         else
           memo << {processed_key, item}
         end
+      end
+    end
+
+    def self.flatten_params(object : JSON::Any, parent_key : String? = nil) : Array(Tuple(String, Crest::ParamsValue))
+      if hash = object.as_h?
+        flatten_params(hash, parent_key)
+      elsif array = object.as_a?
+        flatten_params(array, parent_key)
+      else
+        value : Crest::ParamsValue = nil
+        value = object.as_f? if value.nil?
+        value = object.as_i64? if value.nil?
+        value = object.as_bool? if value.nil?
+        value = object.as_s? if value.nil?
+        [{parent_key, value.as(Crest::ParamsValue)}]
       end
     end
   end
