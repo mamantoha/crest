1a2f594d6d4634ecc3eb87723d13dedee9f7aa4c
Anton Maminov <anton.linux@gmail.com>
2022-10-03 13:17:48 +0300

    test

--- src/ext/http/client.cr
+++ src/ext/http/client.cr
@@ -3,4 +3,9 @@ class HTTP::Client
   def closed? : Bool
     @io ? false : true
   end
+
+  private def exec_internal_single(request, implicit_compression = false)
+    send_request(request)
+    HTTP::Client::Response.from_io?(io, ignore_body: request.ignore_body?, decompress: implicit_compression)
+  end
 end
