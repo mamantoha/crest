5c201d9a64ec2372a1738617f1224c543a82dcf9
Anton Maminov <anton.linux@gmail.com>
2025-11-25 11:51:51 +0200

    Update named_arguments.cr

--- samples/named_arguments.cr
+++ samples/named_arguments.cr
@@ -1,5 +1,5 @@
 require "../src/crest"
 
-reponse = Crest::Request.execute(:get, "http://127.0.0.1:4567/get", invalid: "test")
+Crest::Request.execute(:get, "http://127.0.0.1:4567/get", invalid: "test")
 
 # Compile time error: Error: no parameter named 'invalid'
