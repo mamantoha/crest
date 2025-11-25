b2eb2beb89402c841b4998fc9f50f4f6c000a0ce
Anton Maminov <anton.linux@gmail.com>
2025-11-25 11:46:30 +0200

    allow server in cli mode

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,13 +3,8 @@ require "json"
 require "http_proxy"
 require "vcr"
 require "../src/crest"
+require "./support/constants"
 require "./support/server"
-
-TEST_SERVER_HOST  = "127.0.0.1"
-TEST_SERVER_PORT  = 4567
-TEST_SERVER_URL   = "http://#{TEST_SERVER_HOST}:#{TEST_SERVER_PORT}"
-PROXY_SERVER_HOST = "127.0.0.1"
-PROXY_SERVER_PORT = 8088
 
 def with_proxy_server(host = PROXY_SERVER_HOST, port = PROXY_SERVER_PORT, &)
   wants_close = Channel(Nil).new
