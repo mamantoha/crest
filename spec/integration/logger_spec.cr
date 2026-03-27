1f3fa1b4ae0365c316ea05edd6c2d328f0667389
Anton Maminov <anton.linux@gmail.com>
2026-03-27 14:09:28 +0200

    add a custom logger integration spec

--- spec/integration/logger_spec.cr
+++ spec/integration/logger_spec.cr
@@ -1,5 +1,20 @@
 require "../spec_helper"
 
+private class FilteredLogger < Crest::Logger
+  def initialize(io : IO = STDOUT)
+    super
+    filter(/(access_token=)([^&]+)/, "\\1[REMOVED]")
+  end
+
+  def request(request : Crest::Request) : Nil
+    info(">> | %s | %s" % [request.method, request.url])
+  end
+
+  def response(response : Crest::Response) : Nil
+    info("<< | %s | %s" % [response.status_code, response.url])
+  end
+end
+
 describe Crest::Logger do
   it "logs request and response" do
     IO.pipe do |reader, writer|
@@ -8,12 +23,11 @@ describe Crest::Logger do
 
       Crest::Request.get("#{TEST_SERVER_URL}/get", params: params, logger: logger, logging: true)
 
-      data_time = "\\d{4}-\\d{2}-\\d{2} d{2}:\\d{2}:\\d{2}"
+      date_time = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}"
       url = "#{TEST_SERVER_URL}/get?width=100&height=100"
-      response_body = "\"Width: 100, height: 100\""
 
-      reader.gets.should match(Regex.new("crest | #{data_time} | .* GET.* | #{url}"))
-      reader.gets.should match(Regex.new("crest | #{data_time} | .* 200.* | #{url} | #{response_body}"))
+      reader.gets.should match(Regex.new("\\Acrest \\| #{date_time} \\| .*GET.* \\| #{Regex.escape(url)}\\z"))
+      reader.gets.should match(Regex.new("\\Acrest \\| #{date_time} \\| .*200.* \\| #{Regex.escape(url)} \\| \".*\"\\z"))
     end
   end
 
@@ -26,12 +40,28 @@ describe Crest::Logger do
 
         Crest::Request.get("#{TEST_SERVER_URL}/get", params: params, logger: logger, logging: true)
 
-        data_time = "\\d{4}-\\d{2}-\\d{2} d{2}:\\d{2}:\\d{2}"
+        date_time = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}"
         url = "#{TEST_SERVER_URL}/get?width=100&height=100&access_token=[REMOVED]&v=2"
-        response_body = "\"Width: 100, height: 100\""
 
-        reader.gets.should match(Regex.new("crest | #{data_time} | .* GET.* | #{url}"))
-        reader.gets.should match(Regex.new("crest | #{data_time} | .* 200.* | #{url} | #{response_body}"))
+        reader.gets.should match(Regex.new("\\Acrest \\| #{date_time} \\| .*GET.* \\| #{Regex.escape(url)}\\z"))
+        reader.gets.should match(Regex.new("\\Acrest \\| #{date_time} \\| .*200.* \\| #{Regex.escape(url)} \\| \".*\"\\z"))
+      end
+    end
+  end
+
+  describe "custom loggers" do
+    it "applies filters when subclass logs through info" do
+      IO.pipe do |reader, writer|
+        params = {:width => "100", :access_token => "secret", :height => 100}
+        logger = FilteredLogger.new(writer)
+
+        Crest::Request.get("#{TEST_SERVER_URL}/get", params: params, logger: logger, logging: true)
+
+        date_time = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}"
+        url = "#{TEST_SERVER_URL}/get?width=100&access_token=[REMOVED]&height=100"
+
+        reader.gets.should match(Regex.new("\\Acrest \\| #{date_time} >> \\| GET \\| #{Regex.escape(url)}\\z"))
+        reader.gets.should match(Regex.new("\\Acrest \\| #{date_time} << \\| 200 \\| #{Regex.escape(url)}\\z"))
       end
     end
   end
